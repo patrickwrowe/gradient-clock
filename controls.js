60cf3ea57a2aa534dba4322b78cd345e1a5f5b07
Patrick Rowe <patrick.william.rowe@gmail.com>
2025-02-28 15:23:02 +0000

    add controls javascript file, producing buttons from list of colours and at least connecting to a functoon that prints the colour. Although there's still some funky business going on with the callback

---
+++ controls.js
@@ -0,0 +1,50 @@
+const hourHandColour1Grp = document.getElementById("hour-hand-colour-1");
+const hourHandColour2Grp = document.getElementById("hour-hand-colour-2");
+const minuteHandcolour1Grp = document.getElementById("minute-hand-colour-1");
+const minuteHandcolour2Grp = document.getElementById("minute-hand-colour-2");
+
+const HourHand = document.getElementById("hour-hand");
+const MinuteHand = document.getElementById("minute-hand");
+
+const colours = [
+    "transparent",
+    "black",
+    "white",
+    "#020a23",
+    "#030a23",
+    "#040a23",
+    "#050a23",
+    "#060a23",
+    "#070a23",
+]
+
+const hourHandColour1 = colours[0];
+const hourHandColour2 = colours[1];
+const minuteHandColour1 = colours[0];
+const minuteHandColour2 = colours[1];
+
+const handColourGrps = [
+    hourHandColour1Grp, 
+    hourHandColour2Grp, 
+    minuteHandcolour1Grp, 
+    minuteHandcolour2Grp
+];
+
+buttons = [];
+
+handColourGrps.forEach(hand => {
+    colours.forEach(colour => {
+        id = `${hand.id}-${colour}`
+        hand.innerHTML += `
+            <button class="colour-button" id="${id}" onclick=setHandColours("${colour}")>${colour}</button>    
+        `
+        console.log(hand.id)
+    })
+});
+
+
+function setHandColours (colour) {
+    console.log("firing");
+    console.log(colour);
+}
+
