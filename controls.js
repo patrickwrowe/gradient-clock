277c42e61feaf965a67eec5a87b9c3e416f7bd0c
Patrick Rowe <patrick.william.rowe@gmail.com>
2025-02-28 14:44:36 -0400

    button actually control colour and sliders control extent

--- controls.js
+++ controls.js
@@ -3,25 +3,29 @@ const hourHandColour2Grp = document.getElementById("hour-hand-colour-2");
 const minuteHandcolour1Grp = document.getElementById("minute-hand-colour-1");
 const minuteHandcolour2Grp = document.getElementById("minute-hand-colour-2");
 
-const HourHand = document.getElementById("hour-hand");
-const MinuteHand = document.getElementById("minute-hand");
+let HourHand = document.getElementById("hour-hand");
+let MinuteHand = document.getElementById("minute-hand");
 
+
+/* Colour Definitions */
 const colours = [
     "transparent",
     "black",
     "white",
-    "#020a23",
-    "#030a23",
-    "#040a23",
-    "#050a23",
-    "#060a23",
-    "#070a23",
+    "#32a8a2",
+    "#9432a8",
+    "#4ea832",
+    "#a83250",
+    "#cfa12d",
+    "#cf2d7b",
 ]
 
-const hourHandColour1 = colours[0];
-const hourHandColour2 = colours[1];
-const minuteHandColour1 = colours[0];
-const minuteHandColour2 = colours[1];
+let hourHandColour1 = colours[1];
+let hourHandColour2 = colours[0];
+let hourHandExtent = 100;
+let minuteHandColour1 = colours[1];
+let minuteHandColour2 = colours[0];
+let minuteHandExtent = 20;
 
 const handColourGrps = [
     hourHandColour1Grp, 
@@ -30,8 +34,8 @@ const handColourGrps = [
     minuteHandcolour2Grp
 ];
 
-buttons = [];
 
+/* set up colour buttons */
 handColourGrps.forEach(hand => {
     colours.forEach(colour => {
         id = `${hand.id}-${colour}`
@@ -39,13 +43,60 @@ handColourGrps.forEach(hand => {
         hand.innerHTML += `
             <button class="colour-button" id="${id}" onclick="setHandColours('${hand.id}', '${colour}')">${colour}</button>    
         `
+
+        document.getElementById(id).style.backgroundColor = colour;
     })
 });
 
 
+/* change colours by clicking on buttons */
 function setHandColours (hand, colour) {
-    console.log("firing");
-    console.log(hand);
-    console.log(colour);
+    switch (hand) {
+        case "hour-hand-colour-1":
+            hourHandColour1 = colour;
+            break;
+        case "hour-hand-colour-2":
+            hourHandColour2 = colour;
+            break;
+        case "minute-hand-colour-1":
+            minuteHandColour1 = colour;
+            break;
+        case "minute-hand-colour-2":
+            minuteHandColour2 = colour;
+            break;
+    }
+
+    updateClockGradients();
 }
 
+/* update clock gradients when colours change */
+function updateClockGradients () {
+
+    hourHand.style.backgroundImage = `conic-gradient(
+        ${hourHandColour1},
+        ${hourHandColour2} ${hourHandExtent}%,
+        ${hourHandColour2}
+    )
+    `
+    minuteHand.style.backgroundImage = `conic-gradient(
+        ${minuteHandColour1},
+        ${minuteHandColour2} ${minuteHandExtent}%,
+        ${minuteHandColour2}
+    )
+    `
+}
+
+
+
+const minuteHandExtentSlider = document.getElementById("minute-extent-slider");   
+const hourHandExtentSlider = document.getElementById("hour-extent-slider");
+
+minuteHandExtentSlider.addEventListener("input", updateExtents);
+hourHandExtentSlider.addEventListener("input", updateExtents);
+
+function updateExtents () {
+    hourHandExtent = hourHandExtentSlider.value;
+    minuteHandExtent = minuteHandExtentSlider.value;
+
+    updateClockGradients();
+}
